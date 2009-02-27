29b66828d0ea51635c04e3edbae102ee4156eb4c
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-02-27 15:05:14 +0000

    CLJ-2
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/clojure-plugin@22847 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/clojure/formatter/processors/ClojureIndentProcessor.java
+++ src/org/jetbrains/plugins/clojure/formatter/processors/ClojureIndentProcessor.java
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.clojure.formatter.processors;
 import com.intellij.formatting.Indent;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.TokenSet;
 import org.jetbrains.plugins.clojure.formatter.ClojureBlock;
 import org.jetbrains.plugins.clojure.parser.ClojureElementTypes;
 import org.jetbrains.plugins.clojure.psi.api.ClojureFile;
@@ -21,8 +22,9 @@ public class ClojureIndentProcessor implements ClojureElementTypes{
     }
 
     ASTNode node = parent.getNode();
+    final TokenSet L_BRACES = TokenSet.create(LEFT_CURLY, LEFT_PAREN, LEFT_SQUARE);
     if (LIST_LIKE_FORMS.contains(node.getElementType())) {
-      if (BRACES.contains(child.getElementType())) {
+      if (L_BRACES.contains(child.getElementType())) {
         return Indent.getNoneIndent();
       } else {
         return Indent.getNormalIndent();
