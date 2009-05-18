7495754c9783fb46e4aa4db6e132676bd6425edb
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-05-18 22:31:02 +0000

    selectioner improved
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/clojure-plugin@24359 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/clojure/editor/selection/ClojureListSelectioner.java
+++ src/org/jetbrains/plugins/clojure/editor/selection/ClojureListSelectioner.java
@@ -4,10 +4,7 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.plugins.clojure.psi.ClojurePsiElement;
-import org.jetbrains.plugins.clojure.psi.api.ClList;
-import org.jetbrains.plugins.clojure.psi.api.ClMap;
-import org.jetbrains.plugins.clojure.psi.api.ClSet;
-import org.jetbrains.plugins.clojure.psi.api.ClVector;
+import org.jetbrains.plugins.clojure.psi.api.*;
 
 import java.util.List;
 
@@ -17,14 +14,24 @@ import java.util.List;
 public class ClojureListSelectioner extends ClojureBasicSelectioner {
   public boolean canSelect(PsiElement e) {
     return e instanceof ClList ||
-        e instanceof ClVector ||
-        e instanceof ClSet ||
-        e instanceof ClMap;
+            e instanceof ClVector ||
+            e instanceof ClSet ||
+            e instanceof ClMap;
   }
 
   @Override
-   public List<TextRange> select(PsiElement element, CharSequence editorText, int cursorOffset, Editor editor) {
+  public List<TextRange> select(PsiElement element, CharSequence editorText, int cursorOffset, Editor editor) {
     List<TextRange> result = super.select(element, editorText, cursorOffset, editor);
+    if (element instanceof ClBraced) {
+      ClBraced list = (ClBraced) element;
+      final PsiElement left = list.getFirstBrace();
+      final PsiElement right = list.getLastBrace();
+      if (right != null) {
+        result.add(new TextRange(left.getTextRange().getStartOffset(), right.getTextRange().getEndOffset()));
+      } else {
+        result.add(new TextRange(left.getTextRange().getStartOffset(), element.getTextRange().getEndOffset()));
+      }
+    }
     if (element instanceof ClojurePsiElement) {
       final ClojurePsiElement psi = (ClojurePsiElement) element;
       final PsiElement fst = psi.getFirstNonLeafElement();
@@ -33,6 +40,7 @@ public class ClojureListSelectioner extends ClojureBasicSelectioner {
       final int end = lst != null ? lst.getTextRange().getEndOffset() : psi.getTextRange().getEndOffset();
       result.add(new TextRange(start, end));
     }
+
 
     return result;
   }
