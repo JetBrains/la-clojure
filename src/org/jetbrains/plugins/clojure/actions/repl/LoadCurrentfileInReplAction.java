4372d3aee2695d343d8d3cc60941aa1fc9320226
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-05-18 18:47:33 +0000

    checker
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/clojure-plugin@24350 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/clojure/actions/repl/LoadCurrentfileInReplAction.java
+++ src/org/jetbrains/plugins/clojure/actions/repl/LoadCurrentfileInReplAction.java
@@ -2,17 +2,56 @@ package org.jetbrains.plugins.clojure.actions.repl;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.plugins.clojure.psi.api.ClojureFile;
 
 /**
  * @author ilyas
  */
 public class LoadCurrentfileInReplAction extends ClojureAction {
+  @Override
+  public void update(AnActionEvent e) {
+    final Presentation presentation = e.getPresentation();
+
+    final Editor editor = e.getData(DataKeys.EDITOR);
+
+    if (editor == null) {
+      presentation.setEnabled(false);
+      return;
+    }
+    final Project project = editor.getProject();
+    if (project == null) {
+      presentation.setEnabled(false);
+      return;
+    }
+
+    final Document document = editor.getDocument();
+    final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+    if (psiFile == null || !(psiFile instanceof ClojureFile)) {
+      presentation.setEnabled(false);
+      return;
+    }
+
+    final VirtualFile virtualFile = psiFile.getVirtualFile();
+    if (virtualFile == null) {
+      presentation.setEnabled(false);
+      return;
+    }
+    final String filePath = virtualFile.getPath();
+    if (filePath == null) {
+      presentation.setEnabled(false);
+      return;
+    }
+
+    presentation.setEnabled(true);
+    super.update(e);
+  }
 
   public void actionPerformed(final AnActionEvent e) {
     final Editor editor = e.getData(DataKeys.EDITOR);
@@ -23,7 +62,7 @@ public class LoadCurrentfileInReplAction extends ClojureAction {
 
     final Document document = editor.getDocument();
     final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
-    if (psiFile == null) return;
+    if (psiFile == null || !(psiFile instanceof ClojureFile)) return;
 
     final VirtualFile virtualFile = psiFile.getVirtualFile();
     if (virtualFile == null) return;
