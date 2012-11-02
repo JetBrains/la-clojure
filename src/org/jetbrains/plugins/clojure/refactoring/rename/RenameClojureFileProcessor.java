d181a18cf8d8c05ff1d4d05c0b92805c34003e01
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-11-02 15:02:29 +0400

    Fixed assertion for wrongly implemented extension point.

--- src/org/jetbrains/plugins/clojure/refactoring/rename/RenameClojureFileProcessor.java
+++ src/org/jetbrains/plugins/clojure/refactoring/rename/RenameClojureFileProcessor.java
@@ -1,7 +1,15 @@
 package org.jetbrains.plugins.clojure.refactoring.rename;
 
+import com.intellij.psi.PsiElement;
+import com.intellij.refactoring.rename.RenamePsiElementProcessor;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * @author ilyas
  */
-public class RenameClojureFileProcessor {
+public class RenameClojureFileProcessor extends RenamePsiElementProcessor {
+  @Override
+  public boolean canProcessElement(@NotNull PsiElement element) {
+    return false; //todo: ?
+  }
 }
