e80349c5ada2384defe7d4b539f387b0835f200c
peter <peter@jetbrains.com>
2013-02-07 21:27:02 +0100

    Rainbow parenthesis and commenting #CLJ-163 fixed

--- src/org/jetbrains/plugins/clojure/editor/braceHighlighter/ClojureBraceHighlighter.java
+++ src/org/jetbrains/plugins/clojure/editor/braceHighlighter/ClojureBraceHighlighter.java
@@ -1,10 +1,14 @@
 package org.jetbrains.plugins.clojure.editor.braceHighlighter;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.event.DocumentAdapter;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
@@ -12,6 +16,7 @@ import com.intellij.openapi.editor.event.EditorEventMulticaster;
 import com.intellij.openapi.editor.ex.DocumentEx;
 import com.intellij.openapi.editor.ex.EditorEventMulticasterEx;
 import com.intellij.openapi.editor.ex.FocusChangeListener;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.FileEditorManagerAdapter;
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
@@ -20,99 +25,45 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vcs.FileStatusManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.util.Alarm;
 import com.intellij.util.Processor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.clojure.parser.ClojureElementTypes;
+import org.jetbrains.plugins.clojure.psi.api.ClList;
 import org.jetbrains.plugins.clojure.settings.ClojureProjectSettings;
 
 /**
  * @author ilyas
  */
-public class ClojureBraceHighlighter extends AbstractProjectComponent {
-
-  private final Alarm myAlarm = new Alarm();
-  static Key<Boolean> MUST_BE_REHIGHLIGHTED = new Key<Boolean>("EDITOR_IS_REHIGHLIGHTED");
-
-  @NotNull
-  @Override
-  public String getComponentName() {
-    return "ClojureBraceHighlighter";
-  }
-
-  protected ClojureBraceHighlighter(Project project) {
-    super(project);
-  }
-
-  public void projectOpened() {
-    StartupManager.getInstance(myProject).registerPostStartupActivity(new DumbAwareRunnable() {
-      public void run() {
-        doInit();
-      }
-    });
-  }
-
-  private void doInit() {
-    final EditorEventMulticaster eventMulticaster = EditorFactory.getInstance().getEventMulticaster();
-
-    DocumentListener documentListener = new DocumentAdapter() {
-      public void documentChanged(DocumentEvent e) {
-        myAlarm.cancelAllRequests();
-        Editor[] editors = EditorFactory.getInstance().getEditors(e.getDocument(), myProject);
-        for (Editor editor : editors) {
-          if (editor.getProject() != null) {
-            updateBraces(editor, myAlarm, e);
-          }
+public class ClojureBraceHighlighter implements Annotator {
+  public void annotate(@NotNull PsiElement psiElement, @NotNull AnnotationHolder annotationHolder) {
+    if (psiElement instanceof LeafPsiElement &&
+        ClojureProjectSettings.getInstance(psiElement.getProject()).coloredParentheses) {
+      IElementType type = ((LeafPsiElement) psiElement).getElementType();
+      if (type == ClojureElementTypes.LEFT_PAREN || type == ClojureElementTypes.RIGHT_PAREN) {
+        int level = getLevel(psiElement);
+        if (level >= 0) {
+          final EditorColorsScheme scheme = EditorColorsManager.getInstance().getGlobalScheme();
+          TextAttributes attrs = ClojureBraceAttributes.getBraceAttributes(level, scheme.getDefaultBackground());
+          annotationHolder.createInfoAnnotation(psiElement, "").setEnforcedTextAttributes(attrs);
         }
       }
-    };
-    eventMulticaster.addDocumentListener(documentListener, myProject);
-
-    final FocusChangeListener myFocusChangeListener = new FocusChangeListener() {
-      public void focusGained(Editor editor) {
-        updateBraces(editor, myAlarm, null);
-      }
-
-      public void focusLost(Editor editor) {
-        // do nothing
-      }
-    };
-    ((EditorEventMulticasterEx) eventMulticaster).addFocusChangeListner(myFocusChangeListener);
-
-
-    myProject.getMessageBus().connect(myProject).subscribe(
-        FileEditorManagerListener.FILE_EDITOR_MANAGER,
-        new FileEditorManagerAdapter() {
-          public void selectionChanged(FileEditorManagerEvent e) {
-            myAlarm.cancelAllRequests();
-          }
-        });
-  }
-
-  static void updateBraces(@NotNull final Editor editor, @NotNull final Alarm alarm, final DocumentEvent e) {
-    final Project project = editor.getProject();
-    if (project != null &&
-        !ClojureProjectSettings.getInstance(project).coloredParentheses) {
-      final Boolean data = editor.getUserData(MUST_BE_REHIGHLIGHTED);
-      if (data == null || data) {
-        EditorFactory.getInstance().refreshAllEditors();
-        FileStatusManager.getInstance(project).fileStatusesChanged();
-        DaemonCodeAnalyzer.getInstance(project).settingsChanged();
-      }
-      editor.putUserData(MUST_BE_REHIGHLIGHTED, false);
-      return;
     }
-
-    editor.putUserData(MUST_BE_REHIGHLIGHTED, true);
-
-    final Document document = editor.getDocument();
-    if (document instanceof DocumentEx && ((DocumentEx) document).isInBulkUpdate()) return;
-    ClojureBraceHighlightingHandler.lookForInjectedAndHighlightInOtherThread(editor, alarm, new Processor<ClojureBraceHighlightingHandler>() {
-      public boolean process(final ClojureBraceHighlightingHandler handler) {
-        handler.updateBraces(e);
-        return false;
-      }
-    });
   }
 
+  private static int getLevel(PsiElement psiElement) {
+    int level = -1;
+    PsiElement eachParent = psiElement;
+    while (eachParent != null) {
+      if (eachParent instanceof ClList) {
+        level++;
+      }
+      eachParent = eachParent.getParent();
+    }
+    return level;
+  }
 
 }
