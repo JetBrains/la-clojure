2752d0294975e80bd0d8258100274d294a7ef92c
peter <peter@jetbrains.com>
2013-10-01 10:41:51 +0200

    ClojureLoader cleanup

--- src/java/org/jetbrains/plugins/clojure/ClojureLoader.java
+++ src/java/org/jetbrains/plugins/clojure/ClojureLoader.java
@@ -3,78 +3,16 @@ package org.jetbrains.plugins.clojure;
 import clojure.lang.RT;
 import clojure.lang.Symbol;
 import clojure.lang.Var;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.util.containers.HashSet;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.StringWriter;
-import java.util.Set;
 
-/**
- * Created by IntelliJ IDEA.
- * User: peter
- * Date: Jan 16, 2009
- * Time: 4:34:18 PM
- * Copyright 2007, 2008, 2009 Red Shark Technology
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 public class ClojureLoader implements ApplicationComponent {
-
-  private final Logger LOG = Logger.getLogger(ClojureLoader.class);
-
-  @NotNull
-  public static final String CLOJURE_EXTENSION = "clj";
-
-  @NotNull
-  public static final Set<String> CLOJURE_EXTENSIONS = new HashSet<String>();
+  private static final Logger LOG = Logger.getLogger(ClojureLoader.class);
 
   private static final String INIT_CLOJURE = "org.jetbrains.plugins.clojure.init-clojure";
-
-  static {
-    adjustClojureCompilerLoader();
-
-    CLOJURE_EXTENSIONS.add(CLOJURE_EXTENSION);
-  }
-
-  private static void adjustClojureCompilerLoader() {
-    // Hack in order to adjust Clojure ClassLoaders with PluginClassLoader
-    final Application application = ApplicationManager.getApplication();
-    final ClassLoader loader = ClojureLoader.class.getClassLoader();
-
-    final Runnable runnable = new Runnable() {
-      public void run() {
-        final Thread thread = new Thread() {
-          @Override
-          public void run() {
-            application.invokeLater(new Runnable() {
-              public void run() {
-                Var.pushThreadBindings(RT.map(clojure.lang.Compiler.LOADER, loader));
-              }
-            });
-          }
-        };
-        thread.setContextClassLoader(loader);
-        thread.start();
-      }
-    };
-
-    application.invokeLater(runnable);
-
-  }
-
-
-  public ClojureLoader() {
-  }
 
   public void initComponent() {
     ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
